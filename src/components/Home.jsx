0c4cdc2ad68428d96ab045e2e27de99bfa329d9a
Jimmy Liu <jimmyliu20040@gmail.com>
2025-01-25 15:16:42 -0800

    Searchbar styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Activities from './homeComponents/Activities'
 import Header from './homeComponents/Header'
 import Courses from './homeComponents/Courses'
+import Search from './homeComponents/Search'
 
 export default function Home() {
     return (
-        <div className='grid grid-cols-2'>
+        <div className='grid grid-cols-2 px-10'>
             <div>
-                <Header title='Study App' subtitle='Bringing Students Together' style_h1='text-center transform -translate-x-10' style_h2='transform translate-x-10' />
-                <Courses />
+                <Header title='Study App' subtitle='Bringing Students Together' style_h1='text-center transform -translate-x-20 mx-10' style_h2='transform translate-x-10' />
+                <Search />
 
             </div>
             <div>
---
+++ src/components/homeComponents/Search.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Search() {
+    return (
+        <div className='bg-gray-200 max-w-[70%] h-10 rounded-[50px] m-10 flex items-center'>
+            <p className='m-8'>Search Friend...</p>
+        </div>
+    )
+}
