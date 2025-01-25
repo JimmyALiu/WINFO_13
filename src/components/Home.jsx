d17db056f70c10aeca1010a2eb50ffb75ecc4642
Jimmy Liu <jimmyliu20040@gmail.com>
2025-01-25 15:31:15 -0800

    Adding home buttons

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ import Activities from './homeComponents/Activities'
 import Header from './homeComponents/Header'
 import Courses from './homeComponents/Courses'
 import Search from './homeComponents/Search'
+import HomeButton from './homeComponents/HomeButton'
 
 export default function Home() {
     return (
@@ -10,7 +11,8 @@ export default function Home() {
             <div>
                 <Header title='Study App' subtitle='Bringing Students Together' style_h1='text-center transform -translate-x-20 mx-10' style_h2='transform translate-x-10' />
                 <Search />
-
+                <HomeButton text='' />
+                <HomeButton />
             </div>
             <div>
                 <Activities />
