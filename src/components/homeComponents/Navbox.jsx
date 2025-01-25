e861bc60bceba8e1294db4a52772ffeefbc6bbac
Jimmy Liu <jimmyliu20040@gmail.com>
2025-01-25 12:55:19 -0800

    Formatting Nav

--- src/components/homeComponents/Navbox.jsx
+++ src/components/homeComponents/Navbox.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export default function Navbox() {
+
+export default function Navbox(props) {
+    const { children } = props
     return (
-        <div className='bg-orange-500 rounded-lg w-15 h-15 m-auto'></div>
+        <div className='bg-orange-500 rounded-lg w-12 h-12 mx-auto p-1'>{children}</div>
     )
 }
--- src/components/homeComponents/Navigation.jsx
+++ src/components/homeComponents/Navigation.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbox from './Navbox'
+import home from '../../assets/Home.svg'
+import map from '../../assets/Map.svg'
 
 export default function Navigation() {
+
     return (
-        <div className='bg-gray-300 max-w-20 min-h-screen text-center justify-center items-center'>
-            <Navbox></Navbox>
+        <div className='flex flex-col rounded-r-lg bg-gray-300 max-w-20 min-h-screen items-start gap-2 py-3'>
+            <Navbox><img src={home} alt='home' /></Navbox>
+            <Navbox><img src={map} alt='home' /></Navbox>
         </div>
     )
 }
