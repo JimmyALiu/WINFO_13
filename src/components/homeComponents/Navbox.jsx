a40b6c2e12ee9bd5ea3b404f0e5c797e066cb716
Jimmy Liu <jimmyliu20040@gmail.com>
2025-01-25 12:25:58 -0800

    Moving svgs to assets

--- src/components/homeComponents/Navbox.jsx
+++ src/components/homeComponents/Navbox.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Navbox() {
     return (
-        <div className='bg-orange-500 rounded-lg w-15 h-15 m-auto'>Text</div>
+        <div className='bg-orange-500 rounded-lg w-15 h-15 m-auto'></div>
     )
 }
