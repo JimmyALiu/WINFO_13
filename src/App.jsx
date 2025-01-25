2577692d4c4456303b247a5f8aacdaf0da3a84a0
GabiP13 <64442012+GabiP13@users.noreply.github.com>
2025-01-25 11:52:29 -0800

    app login changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import { useState } from 'react'
-import Login from './components/Login'
+// import Login from './components/Login'
 
 function App() {
-  const [ pageIndex, setPageIndex ] = useState(0)
-  
-  return (
-    <>
-		<Login></Login>
-    </>
-  )
+	const [pageIndex, setPageIndex] = useState(0)
+
+	switch (pageIndex) {
+		case 1:
+			// return 
+			break;
+		case 2:
+			// code block
+			break;
+		default:
+			// return (
+			// 	<div>
+			// 		<h1>Name of App</h1>
+			// 		<Login></Login>
+			// 	</div>
+			// )
+			return <p>homepage</p>
+	}
 }
 
 export default App
