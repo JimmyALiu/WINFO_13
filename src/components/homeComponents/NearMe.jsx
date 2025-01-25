adbe3a8b7c12a71a2f64d4a165a33cff4c8ea4f3
GabiP13 <64442012+GabiP13@users.noreply.github.com>
2025-01-25 15:56:58 -0800

    nearme further edits + index dots

--- src/components/homeComponents/NearMe.jsx
+++ src/components/homeComponents/NearMe.jsx
@@ -11,21 +11,43 @@ export default function NearMe() {
                 </h1>
             </div> */}
 
-            <Header title='Friends Near Me' subtitle='> Find fellow classmates or other students studying near you!'></Header>
+            <Header title='Friends Near Me' subtitle='> Find fellow classmates or other students studying near you!' style_h2='transform -translate-x-70'></Header>
 
             <div className='pt-10, h-100%'>
 
                 <div className="grid grid-flow-col gap-3">
-                    <div className="bg-blue-100 col-span-23">
+                    <div className="bg-[#e7f0fd] col-span-23">
                         <div className='m-4'>
-                            <h1>Active now</h1>
-                            <div>
+                            <div className='pb-7'>
+                                <h3>Active now:</h3>
+                                <div className="grid grid-flow-col gap-3 m-6">
+                                    <div className="dot col-span-1"></div>
+                                    <div className="m-1.5 col-span-3">
+                                        <h2>Name of Individual</h2>
+                                    </div>
+                                </div>
+                                <div className="grid grid-flow-col gap-3 m-6">
+                                    <div className="dot col-span-1"></div>
+                                    <div className="m-1.5 col-span-3">
+                                        <h2>Name of Individual2</h2>
+                                    </div>
+                                </div>
+                                <div className="grid grid-flow-col gap-3 m-6">
+                                    <div className="dot col-span-1"></div>
+                                    <div className="m-1.5 col-span-3">
+                                        <h2>Name of Individual3</h2>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='pt-3 pb-7'>
+                                <h3>Location:</h3>
                                 
                             </div>
                         </div>
                     </div>
-                    <div class="bg-red-100 col-span-1">
-                        <img className='m-4 h-[700px]' src={UWmap}/>
+                    <div class="bg-[#f3e9fc] col-span-1">
+                        <img className='my-3 ml-3 mr-2 h-[735px]' src={UWmap}/>
                     </div>
                 </div>
                 
