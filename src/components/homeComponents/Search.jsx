4bb51d83d3403d6799f5b65ba2f0b26122710499
Jimmy Liu <jimmyliu20040@gmail.com>
2025-01-25 15:18:43 -0800

    Refractor colors

--- src/components/homeComponents/Search.jsx
+++ src/components/homeComponents/Search.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Search() {
     return (
         <div className='bg-gray-200 max-w-[70%] h-10 rounded-[50px] m-10 flex items-center'>
-            <p className='m-8 k2d'>Search Friend...</p>
+            <p className='m-8 k2d orange'>Search Friend...</p>
         </div>
     )
 }
