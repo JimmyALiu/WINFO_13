059a01711380c0b042a5a7d75f86375967f1fc0b
Jimmy Liu <jimmyliu20040@gmail.com>
2025-01-25 13:19:33 -0800

    Pass down page index

--- src/components/homeComponents/Navigation.jsx
+++ src/components/homeComponents/Navigation.jsx
@@ -3,12 +3,13 @@ import Navbox from './Navbox'
 import home from '../../assets/Home.svg'
 import map from '../../assets/Map.svg'
 
-export default function Navigation() {
+export default function Navigation(props) {
+    const { setPageIndex } = props
 
     return (
         <div className='flex flex-col rounded-r-lg bg-gray-300 w-20 min-h-screen items-start gap-2 py-3'>
-            <Navbox><img src={home} alt='home' /></Navbox>
-            <Navbox><img src={map} alt='home' /></Navbox>
+            <Navbox><img src={home} alt='home' setPageIndex={setPageIndex} /></Navbox>
+            <Navbox><img src={map} alt='home' setPageIndex={setPageIndex} /></Navbox>
             <hr className='min-w-15 my-5 mx-auto bg-gray-200 border-1 dark:bg-gray-700' />
         </div>
     )
