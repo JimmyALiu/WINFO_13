12200c627c44c456eec656bfcfac7b0909b90177
Jimmy Liu <jimmyliu20040@gmail.com>
2025-01-25 14:54:50 -0800

    Update Header.jsx

--- src/components/homeComponents/Header.jsx
+++ src/components/homeComponents/Header.jsx
@@ -5,9 +5,9 @@ export default function Header(props) {
 
     return (
     
-        <div>
-            <h1 className='font-bold text-[70px] max-h-22 text-center mx-10 uppercase '>{title}</h1>
-            <h2 className='font-thin text-[22px] text-center mt-0 min-h-auto leading-none'>{subtitle}</h2>
+        <div className='text-center mx-4'>
+            <h1 className='font-bold text-[70px] max-h-22 mx-10 uppercase transform -translate-x-10'>{title}</h1>
+            <h2 className='font-thin text-[22px] text-center mt-0 min-h-auto leading-none transform translate-x-10'>{subtitle}</h2>
         </div>
     )
 }
