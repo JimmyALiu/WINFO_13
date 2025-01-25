ca7680a00b23ac5770c229a37eb1f5ab879b2502
Jimmy Liu <jimmyliu20040@gmail.com>
2025-01-25 12:58:04 -0800

    Update Navigation.jsx

--- src/components/homeComponents/Navigation.jsx
+++ src/components/homeComponents/Navigation.jsx
@@ -6,9 +6,15 @@ import map from '../../assets/Map.svg'
 export default function Navigation() {
 
     return (
-        <div className='flex flex-col rounded-r-lg bg-gray-300 max-w-20 min-h-screen items-start gap-2 py-3'>
-            <Navbox><img src={home} alt='home' /></Navbox>
-            <Navbox><img src={map} alt='home' /></Navbox>
+        <div>
+            <div className='flex flex-col rounded-r-lg bg-gray-300 max-w-20 min-h-screen items-start gap-2 py-3'>
+                <Navbox><img src={home} alt='home' /></Navbox>
+                <Navbox><img src={map} alt='home' /></Navbox>
+            </div>
+
+            <div>
+                
+            </div>
         </div>
     )
 }
