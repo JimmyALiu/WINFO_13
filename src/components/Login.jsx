8a55c02d6dcde9fe1c9ddde1f63ddcda82efe84e
GabiP13 <64442012+GabiP13@users.noreply.github.com>
2025-01-25 11:13:22 -0800

    login button

---
+++ src/components/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Login() {
+    return (
+        <div className='login'>
+            {/* onClick={()} */}
+            <button>
+                <h3>Login with your school ID</h3>
+            </button>
+        </div>
+    )
+}
