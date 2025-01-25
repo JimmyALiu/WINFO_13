e073c2fb6edb01fadf2dd752b0a3ac46e4caa4a5
Jimmy Liu <jimmyliu20040@gmail.com>
2025-01-25 11:49:26 -0800

    Removing Login

--- src/components/homeComponents/Header.jsx
+++ src/components/homeComponents/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
 export default function Header(props) {
+    const { title, subtitle } = props
 
     return (
         <div>
-            <h1>Study App</h1>
+            <h1>{title}</h1>
+            <h2>{subtitle}</h2>
         </div>
     )
 }
