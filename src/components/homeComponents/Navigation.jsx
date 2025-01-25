33516cc5b7847965b251dd651ebd223dcf6b0457
Jimmy Liu <jimmyliu20040@gmail.com>
2025-01-25 12:10:25 -0800

    Update Navigation.jsx

--- src/components/homeComponents/Navigation.jsx
+++ src/components/homeComponents/Navigation.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbox from ''
 
 export default function Navigation() {
     return (
         <div className='bg-gray-300 max-w-20 min-h-screen'>
-            
+
         </div>
     )
 }
