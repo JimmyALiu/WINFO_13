cea107a0f983511e09e955631bccbb34bfe5fe48
Jimmy Liu <jimmyliu20040@gmail.com>
2025-01-25 12:16:02 -0800

    nav changes

--- src/components/homeComponents/Navigation.jsx
+++ src/components/homeComponents/Navigation.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Navbox from ''
+import Navbox from './Navbox'
 
 export default function Navigation() {
     return (
-        <div className='bg-gray-300 max-w-20 min-h-screen'>
-
+        <div className='bg-gray-300 max-w-20 min-h-screen text-center justify-center items-center'>
+            <Navbox></Navbox>
         </div>
     )
 }
