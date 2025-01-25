10f585d476e428645ad00dc0f829e62d93ce6b50
Jimmy Liu <jimmyliu20040@gmail.com>
2025-01-25 11:36:03 -0800

    Tab formatting

--- src/components/homeComponents/Header.jsx
+++ src/components/homeComponents/Header.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
-export default function Header() {
-  return (
-    <div>Header</div>
-  )
+export default function Header(props) {
+
+    return (
+        <div>
+            <h1>Study App</h1>
+        </div>
+    )
 }
