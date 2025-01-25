ab87b20e79d92716c20b7b58e2e2836ee5201eaa
Jimmy Liu <jimmyliu20040@gmail.com>
2025-01-25 13:09:16 -0800

    adding buttons to icons

--- src/components/homeComponents/Navbox.jsx
+++ src/components/homeComponents/Navbox.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 
 export default function Navbox(props) {
-    const { children } = props
+    const { children, clickFunc } = props
     return (
-        <div className='bg-orange-500 rounded-lg w-12 h-12 mx-auto p-1'>{children}</div>
+        <div className='bg-orange-500 rounded-lg w-12 h-12 mx-auto p-1'>
+            <button onClick={(clickFunc)}>{children}</button>
+        </div>
     )
 }
--- src/components/homeComponents/Navigation.jsx
+++ src/components/homeComponents/Navigation.jsx
@@ -6,15 +6,10 @@ import map from '../../assets/Map.svg'
 export default function Navigation() {
 
     return (
-        <div>
-            <div className='flex flex-col rounded-r-lg bg-gray-300 max-w-20 min-h-screen items-start gap-2 py-3'>
-                <Navbox><img src={home} alt='home' /></Navbox>
-                <Navbox><img src={map} alt='home' /></Navbox>
-            </div>
-
-            <div>
-                
-            </div>
+        <div className='flex flex-col rounded-r-lg bg-gray-300 max-w-20 min-h-screen items-start gap-2 py-3'>
+            <Navbox><img src={home} alt='home' /></Navbox>
+            <Navbox><img src={map} alt='home' /></Navbox>
+            <hr className='min-w-15 my-5 mx-auto bg-gray-200 border-1 dark:bg-gray-700' />
         </div>
     )
 }
