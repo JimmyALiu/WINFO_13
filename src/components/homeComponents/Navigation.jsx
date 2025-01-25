4bc69225534f32d896272e259193e6696cdaafd4
Jimmy Liu <jimmyliu20040@gmail.com>
2025-01-25 13:12:57 -0800

    Update Navigation.jsx

--- src/components/homeComponents/Navigation.jsx
+++ src/components/homeComponents/Navigation.jsx
@@ -6,7 +6,7 @@ import map from '../../assets/Map.svg'
 export default function Navigation() {
 
     return (
-        <div className='flex flex-col rounded-r-lg bg-gray-300 max-w-20 min-h-screen items-start gap-2 py-3'>
+        <div className='flex flex-col rounded-r-lg bg-gray-300 w-20 min-h-screen items-start gap-2 py-3'>
             <Navbox><img src={home} alt='home' /></Navbox>
             <Navbox><img src={map} alt='home' /></Navbox>
             <hr className='min-w-15 my-5 mx-auto bg-gray-200 border-1 dark:bg-gray-700' />
