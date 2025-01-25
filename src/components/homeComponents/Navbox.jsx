b6ae4ccb4a8769c15c87c18c1478c50e7316df1f
Jimmy Liu <jimmyliu20040@gmail.com>
2025-01-25 14:21:21 -0800

    Added button functionality

--- src/components/homeComponents/Navbox.jsx
+++ src/components/homeComponents/Navbox.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 
 export default function Navbox(props) {
-    const { children, clickFunc } = props
+    const { children, setPageIndex, index } = props
     return (
         <div className='bg-orange-500 rounded-lg w-12 h-12 mx-auto p-1'>
-            <button onClick={(clickFunc)}>{children}</button>
+            <button onClick={() => setPageIndex(index) }>
+                {children}
+            </button>
         </div>
     )
 }
--- src/components/homeComponents/Navigation.jsx
+++ src/components/homeComponents/Navigation.jsx
@@ -8,8 +8,8 @@ export default function Navigation(props) {
 
     return (
         <div className='flex flex-col rounded-r-lg bg-gray-300 w-20 min-h-screen items-start gap-2 py-3'>
-            <Navbox><img src={home} alt='home' setPageIndex={setPageIndex} /></Navbox>
-            <Navbox><img src={map} alt='home' setPageIndex={setPageIndex} /></Navbox>
+            <Navbox setPageIndex={setPageIndex} index={0}><img src={home} alt='home' /></Navbox>
+            <Navbox setPageIndex={setPageIndex} index={1}><img src={map} alt='map' /></Navbox>
             <hr className='min-w-15 my-5 mx-auto bg-gray-200 border-1 dark:bg-gray-700' />
         </div>
     )
