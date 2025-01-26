e12e67ff07030c31db3de71b0bc869561ff08ff1
Jimmy Liu <jimmyliu20040@gmail.com>
2025-01-25 18:42:31 -0800

    Update Messages.jsx

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import Header from './homeComponents/Header'
 
 export default function Messages() {
     return (
-        <div>Messages</div>
+        <Header subtitle='In Progress; if we had more time this is where students would reach out to each other 1-to-1 or form group chats!' />
     )
 }
