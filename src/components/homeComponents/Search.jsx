3f69bc02347fa1b112b1cee35c3d5eed78acddc3
Jimmy Liu <jimmyliu20040@gmail.com>
2025-01-25 16:17:37 -0800

    Update Search.jsx

--- src/components/homeComponents/Search.jsx
+++ src/components/homeComponents/Search.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 
 export default function Search() {
     return (
-        <div className='bg-gray-200 max-w-[70%] h-10 rounded-[50px] m-10 flex items-center'>
-            <p className='m-8 k2d orange'>Search Friend...</p>
-        </div>
+        <input className='bg-gray-200 max-w-[70%] h-10 rounded-[50px] m-10 flex items-center m-8 k2d orange w-full p-5' placeholder="Search Friends..." />
     )
 }
