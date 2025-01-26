29e9c0e861001c08f27c7b240d7cfa3302e02dcb
GabiP13 <64442012+GabiP13@users.noreply.github.com>
2025-01-25 16:31:33 -0800

    nearme still

--- src/components/homeComponents/NearMe.jsx
+++ src/components/homeComponents/NearMe.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Header from './Header'
 import UWmap from '../imgs/UWmap.png'
+import redellipse from '../imgs/Ellipse 8.png'
+import whiteellipse from '../imgs/Ellipse 9.png'
 
 export default function NearMe() {
     return (
@@ -20,37 +22,60 @@ export default function NearMe() {
                         <div className='m-4'>
                             <div className='pb-7'>
                                 <h3>Active now:</h3>
-                                <div className="grid grid-flow-col gap-3 m-6">
+                                <div className="grid grid-cols-[max-content_2fr] m-6">
                                     <div className="dot col-span-1"></div>
-                                    <div className="m-1.5 col-span-3">
-                                        <h2>Name of Individual</h2>
+                                    <div className="m-1.5 pl-6">
+                                        <h2>Lia Mar-Lundeen</h2>
                                     </div>
                                 </div>
-                                <div className="grid grid-flow-col gap-3 m-6">
+                                <div className="grid grid-cols-[max-content_2fr] m-6">
                                     <div className="dot col-span-1"></div>
-                                    <div className="m-1.5 col-span-3">
-                                        <h2>Name of Individual2</h2>
+                                    <div className="m-1.5 pl-6">
+                                        <h2>Izzy Saccone</h2>
                                     </div>
                                 </div>
-                                <div className="grid grid-flow-col gap-3 m-6">
+                                <div className="grid grid-cols-[max-content_2fr] m-6">
                                     <div className="dot col-span-1"></div>
-                                    <div className="m-1.5 col-span-3">
-                                        <h2>Name of Individual3</h2>
+                                    <div className="m-1.5 pl-6">
+                                        <h2>Gabriela Pletosu</h2>
+                                    </div>
+                                </div>
+                                <div className="grid grid-cols-[max-content_2fr] m-6">
+                                    <div className="dot col-span-1"></div>
+                                    <div className="m-1.5 pl-6">
+                                        <h2>Jimmy Liu</h2>
                                     </div>
                                 </div>
                             </div>
 
                             <div className='pt-3 pb-7'>
-                                <h3>Location:</h3>
-                                
+                                <h3 className='pb-6'>Location:</h3>
+                                <div className='px-8'>
+                                    <div className='pb-3'>
+                                        <input type="checkbox" id="location1" name="location1" value="Odegaard"/>
+                                        <label className='pl-4' for="location">Odegaard</label><br />
+                                    </div>
+                                    <div className='pb-3'>
+                                        <input type="checkbox" id="loc2" name="location2" value="Suzzallo" />
+                                        <label className='pl-4' for="location2">Suzzallo</label><br />
+                                    </div>
+                                    <div className='pb-3'>
+                                        <input type="checkbox" id="location3" name="location3" value="Population Health" />
+                                        <label className='pl-4' for="location3">Population Health</label><br />
+                                    </div> 
+                                </div>
+
                             </div>
                         </div>
                     </div>
                     <div class="bg-[#f3e9fc] col-span-1">
-                        <img className='my-3 ml-3 mr-2 h-[735px]' src={UWmap}/>
+                        {/* <button>
+                            <img src={whiteellipse} />
+                        </button> */}
+                        <img className='my-3 ml-3 mr-2 h-[735px]' src={UWmap} />
                     </div>
                 </div>
-                
+
             </div>
 
 
